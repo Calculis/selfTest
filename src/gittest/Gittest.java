c3ac07051a5eca58c77b0014fb248d0d673ca98f
Phunon <Phunon.sriv@mail.kmutt.ac.th>
2020-10-20 20:05:13 +0700

    Update sayHello()

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -16,6 +16,10 @@ public class Gittest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        sayHello();
+       
+    }
+    public static void sayHello(){
         System.out.println("Hello World");
     }
     
